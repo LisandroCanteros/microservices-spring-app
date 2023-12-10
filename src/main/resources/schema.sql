4a94f303e8d6e7ff9c786c2a1310ea70dbf34da0
Lisandro Canteros <84427419+LisandroCanteros@users.noreply.github.com>
2023-12-10 14:23:36 -0300

    init

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS question (
+    id int PRIMARY KEY,
+    category varchar(100) NOT NULL,
+    difficulty_level varchar(20) NOT NULL,
+    title varchar(100) NOT NULL,
+    option1 varchar(100) NOT NULL,
+    option2 varchar(100) NOT NULL,
+    option3 varchar(100),
+    option4 varchar(100),
+    correct_option varchar(100) NOT NULL
+);
